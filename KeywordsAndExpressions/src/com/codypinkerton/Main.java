64eda6c59dee38828623ce4289627911463bfe4c
Cody <codybjhs@Yahoo.com>
2018-03-30 12:25:22 -0500

    Adding my 4 projects from Tim Buchalkas Java class that i did at home.

---
+++ KeywordsAndExpressions/src/com/codypinkerton/Main.java
@@ -0,0 +1,23 @@
+package com.codypinkerton;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        // a mile is equal to 1.609344 kilometres
+        double kilometers = (100 * 1.609344);
+
+        int highScore = 50;
+        if(highScore == 50)
+        {
+            System.out.println("This is an expression");
+        }
+
+        // score = 100
+        // score > 90
+        // "You got the high score!"
+        // score = 0
+
+    }
+}
---
+++ StatementsWhiteSapceAndIndenting/src/com/codypinkerton/Main.java
@@ -0,0 +1,21 @@
+package com.codypinkerton;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is" +
+                " another" +
+                " still more");
+
+        int anotherVariable = 50;
+        myVariable--;
+        System.out.printf("This is another one");
+    }
+}
